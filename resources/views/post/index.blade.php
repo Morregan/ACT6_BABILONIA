68312b8167d3d640a1fe160bae17da4e76917209
Morregan <MorreganSmith@gmail.com>
2024-04-02 18:00:07 +0800

    commit msg

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actvity 6 Babilonia</title>
+</head>
+<body>
+    <h1>My Activity 6</h1>
+    <ul>
+        <li><a href = "/post/1">Post 1</a></li>
+        <li><a href = "/post/2">Post 2</a></li>
+    </ul>
+</body>
+</html>
